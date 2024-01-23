9b9afbeddda288c1149592bfaa9e0cc246c01729
musta20 <saif.muh2020@gmail.com>
2024-01-23 06:23:47 +0200

    updating stubs

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,29 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Carbon\CarbonInterface;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
+/**
+ * @property string @id
+ * 
+ * @property null|CarbonInterface $created_at
+ * @property null|CarbonInterface $updated_at
+ */
 
+final class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use HasUlids;
+
+    
     /**
      * The attributes that are mass assignable.
      *
